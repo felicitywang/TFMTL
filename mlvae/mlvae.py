a2e15b26287904ada3ad236fc4b22d2700b10922
Seth Ebner <sebner1@jhu.edu>
2018-01-23 12:34:08 -0500

    Support reuse of z samples; remove feature_set

--- mlvae/mlvae.py
+++ mlvae/mlvae.py
@@ -108,14 +108,14 @@ def MLP_ordinal(inputs):
 
 class MultiLabel(object):
   def __init__(self,
-               feature_set=None,
                class_sizes=None,
                dataset_order=None,
                decoder=None,
                hp=None):
 
-    # feature_set: set containing names of features
     # class_sizes: map from feature names to cardinality of label sets
+    # dataset_order: list of features in some fixed order
+    #   (concatenated order matters for decoding)
 
     # Save hyper-parameters.
     if hp is None:
@@ -126,6 +126,8 @@ class MultiLabel(object):
     self._class_sizes = class_sizes
 
     self._dataset_order = dataset_order
+
+    assert class_sizes.keys() == set(dataset_order)  # all feature names are present and consistent across data structures
 
     self._decoder = decoder
 
@@ -145,12 +147,12 @@ class MultiLabel(object):
                                      embed_dim=hp.word_embed_dim)
     # Generative networks
     self._py_templates = dict()
-    for k in feature_set:
-      self._py_templates[k] = tf.make_template('py_{}'.format(k), MLP_unnormalized_log_categorical, output_size=class_sizes[k])
+    for k, v in class_sizes.items():
+      self._py_templates[k] = tf.make_template('py_{}'.format(k), MLP_unnormalized_log_categorical, output_size=v)
     # Inference networks
     self._qy_templates = dict()
-    for k in feature_set:
-      self._qy_templates[k] = tf.make_template('qy_{}'.format(k), MLP_unnormalized_log_categorical, output_size=class_sizes[k])
+    for k, v in class_sizes.items():
+      self._qy_templates[k] = tf.make_template('qy_{}'.format(k), MLP_unnormalized_log_categorical, output_size=v)
     self._qz_template = tf.make_template('qz', MLP_gaussian_posterior)
 
     self._tau = get_tau(hp, decay=hp.decay_tau)
@@ -225,35 +227,37 @@ class MultiLabel(object):
     grads = tf.gradients(loss, tvars)
     return (tvars, grads)
 
-  def get_Eq_log_pz(self, zm, zv, zm_prior, zv_prior):
+  def get_Eq_log_pz(self, zm, zv, zm_prior, zv_prior, z_samples=None):
     res = 0
-    for _ in range(hp.num_z_samples):
-      z = gaussian_sample(zm, zv)
+    if z_samples is None:
+      z_samples = [gaussian_sample(zm, zv) for _ in range(hp.num_z_samples)]
+    for z in z_samples:
       res += log_normal(z, zm_prior, zv_prior)
-    Eq_log_pz = res / hp.num_z_samples
+    Eq_log_pz = res / len(z_samples)
     return Eq_log_pz
 
-  def get_Eq_log_qz(self, zm, zv):
+  def get_Eq_log_qz(self, zm, zv, z_samples=None):
     res = 0
-    for _ in range(hp.num_z_samples):
-      z = gaussian_sample(zm, zv)
+    if z_samples is None:
+      z_samples = [gaussian_sample(zm, zv) for _ in range(hp.num_z_samples)]
+    for z in z_samples:
       res += log_normal(z, zm, zv)
-    Eq_log_qz = res / hp.num_z_samples
+    Eq_log_qz = res / len(z_samples)
     return Eq_log_qz
 
-  def get_kl_qp(self, features, feature_name, feature_dict, observed_dict, zm, zv):
+  def get_kl_qp(self, features, feature_name, feature_dict, observed_dict, zm, zv, z_samples=None):
     kl_qp = 0
     if observed_dict[feature_name] is True:
       # feature is observed (in all examples in this batch)
-      # Decouples into q and p terms (linearity of expectation)
+      # Decouples into q and p terms (linearity of expectation), so we calculate the q and p terms separately
       kl_qp += self.get_Eq_log_qy(feature_name, zm, zv) - self.get_Eq_log_py(features, feature_name, feature_dict, observed_dict, zm, zv)
     else:
       if hp.expectation == 'exact':
-        # Does not easily decouple into q and p terms
+        # Does not easily decouple into q and p terms, so we calculate kl as a whole here
         res = 0
-        for i in range(hp.num_z_samples):
-          z = gaussian_sample(zm, zv)
-          
+        if z_samples is None:
+          z_samples = [gaussian_sample(zm, zv) for _ in range(hp.num_z_samples)]
+        for z in z_samples:          
           qy_logits = self._qy_templates[feature_name](features + [z])
           qcat = Categorical(logits=qy_logits, name='qy_{}_{}_cat'.format(feature_name, i))
           
@@ -262,32 +266,34 @@ class MultiLabel(object):
           
           kl = kl_divergence(qcat, pcat)
           res += kl
-        kl_qp += res / hp.num_z_samples  # average KL divergence between q and p for feature k
+        kl_qp += res / len(z_samples)  # average KL divergence between q and p for feature k
       elif hp.expectation == 'sample':
-        # Decouples into q and p terms (linearity of expectation)
+        # Decouples into q and p terms (linearity of expectation), so we calculate the q and p terms separately
         kl_qp += self.get_Eq_log_qy(feature_name, zm, zv) - self.get_Eq_log_py(features, feature_name, feature_dict, observed_dict, zm, zv)
       else:
         raise ValueError('unrecognized expectation mode: %s' % (hp.expectation))
 
     return kl_qp
 
-  def get_Eq_log_py(self, features, feature_name, feature_dict, observed_dict, zm, zv):
+  def get_Eq_log_py(self, features, feature_name, feature_dict, observed_dict, zm, zv, z_samples=None):
     Eq_log_py = 0
     if observed_dict[feature_name] is True:
       # feature is observed (in all examples in this batch)
       res_p = 0
-      for _ in range(hp.num_z_samples):
-        z = gaussian_sample(zm, zv)
+      if z_samples is None:
+        z_samples = [gaussian_sample(zm, zv) for _ in range(hp.num_z_samples)]
+      for z in z_samples:
         log_probs = self.get_label_log_probability(features, z, feature_name, feature_dict[feature_name], distribution_type='p')
         res_p += log_probs
-      Eq_log_py = res_p / hp.num_z_samples
+      Eq_log_py = res_p / len(z_samples)
     else:
       if hp.expectation == 'exact':
-        assert False  # this branch should not be reachable
+        assert False  # this branch should not be reachable because kl for the exact case is calculated in get_kl_qp()
       elif hp.expectation == 'sample':
         res = 0
-        for i in range(hp.num_z_samples):
-          z = gaussian_sample(zm, zv)
+        if z_samples is None:
+          z_samples = [gaussian_sample(zm, zv) for _ in range(hp.num_z_samples)]
+        for z in z_samples:
           qy_logits = self._qy_templates[feature_name](features + [z])
           qy_concrete = ExpRelaxedOneHotCategorical(self._tau,
                                                     logits=qy_logits,
@@ -300,40 +306,42 @@ class MultiLabel(object):
             # y_preds = tf.one_hot(y_preds, class_sizes[k])
             res_p = pcat.log_prob(y_pred)  # log p(y_samp)
             res += res_p
-        Eq_log_py = res / (hp.num_z_samples * hp.num_y_samples)
+        Eq_log_py = res / (len(z_samples) * hp.num_y_samples)
       else:
         raise ValueError('unrecognized expectation mode: %s' % (hp.expectation))
     return Eq_log_py
 
-  def get_Eq_log_qy(self, features, feature_name, observed_dict, zm, zv):
+  def get_Eq_log_qy(self, features, feature_name, observed_dict, zm, zv, z_samples=None):
     Eq_log_qy = 0
     if observed_dict[feature_name] is True:
       # feature is observed (in all examples in this batch)
       Eq_log_qy = 0  # entropy of a degenerate point-mass (one-hot) probability distribution is 0
     else:
       if hp.expectation == 'exact':
-        assert False  # this branch should not be reachable
+        assert False  # this branch should not be reachable because kl for the exact case is calculated in get_kl_qp()
       elif hp.expectation == 'sample':
         res = 0
-        for i in range(hp.num_z_samples):
-          z = gaussian_sample(zm, zv)
+        if z_samples is None:
+          z_samples = [gaussian_sample(zm, zv) for _ in range(hp.num_z_samples)]
+        for z in z_samples:
           qy_logits = self._qy_templates[feature_name](features + [z])
           qy_concrete = ExpRelaxedOneHotCategorical(self._tau,
-                                                    logits=qy_logits,
+                                                    logits=qy_logits,  # logits do *not* need to be manually exp-normalized for this distribution (the distribution automatically exp-normalizes)
                                                     name='qy_{}_{}_concrete'.format(feature_name, i))
           for _ in range(hp.num_y_samples):
             y_sample = tf.exp(qy_concrete.sample())  # each row is a continuous approximation to a categorical one-hot vector over label values
             # TODO: do we need to create a qcat here and find the (log) probability of the y_pred according to qcat (like we do to calculate Eq_log_py)?
             res += qy_concrete.log_prob(y_sample)  # log q(y_sample)
-        Eq_log_qy = res / (hp.num_z_samples * hp.num_y_samples)
+        Eq_log_qy = res / (len(z_samples) * hp.num_y_samples)
       else:
         raise ValueError('unrecognized expectation mode: %s' % (hp.expectation))
     return Eq_log_qy
 
-  def get_Eq_log_px(self, targets, features, feature_dict, observed_dict, zm, zv):
+  def get_Eq_log_px(self, targets, features, feature_dict, observed_dict, zm, zv, z_samples=None):
     res = 0
-    for _ in range(hp.num_z_samples):
-      z = gaussian_sample(zm, zv)
+    if z_samples is None:
+      z_samples = [gaussian_sample(zm, zv) for _ in range(hp.num_z_samples)]
+    for z in z_samples:
       for _ in range(hp.num_y_samples):
         instantiation = self.get_label_instantiation(features, z, feature_dict, observed_dict)
         # instantiation dict to list with canonical ordering
@@ -341,13 +349,17 @@ class MultiLabel(object):
         markov_blanket = tf.concat([instantiation_list], axis=1)  # parents of x in p model
         nll = self._decoder(targets, markov_blanket)  # reconstruction loss
         res += nll
-    Eq_log_px = res / (hp.num_z_samples * hp.num_y_samples)
+    Eq_log_px = res / (len(z_samples) * hp.num_y_samples)
 
-  def get_disc_loss(self, features, feature_dict, feature_name, observed_dict, zm, zv):
+  def get_disc_loss(self, features, feature_dict, feature_name, observed_dict, zm, zv, z_samples=None):
     disc_loss = 0
     if observed_dict[feature_name] is True:
-      z = gaussian_sample(zm, zv)
-      disc_loss = self.get_label_log_probability(feature_dict, features, z, feature_name, feature_dict[feature_name], distribution_type='q')
+      res = 0
+      if z_samples is None:
+        z_samples = [gaussian_sample(zm, zv) for _ in range(hp.num_z_samples)]
+      for z in z_samples:
+        res += self.get_label_log_probability(feature_dict, features, z, feature_name, feature_dict[feature_name], distribution_type='q')
+      disc_loss = res / len(z_samples)
     else:
       # TODO: does anything need to happen in this case?
       pass
@@ -381,6 +393,8 @@ class MultiLabel(object):
 
     zm, zv = self._qz_template(features)
 
+    # z = gaussian_sample(zm, zv)
+
     Eq_log_pz = self.get_Eq_log_pz(zm, zv, self._zm_prior, self._zv_prior)
     total_kl_qp = 0
     for k in feature_dict:
@@ -396,24 +410,17 @@ class MultiLabel(object):
 
     # maximize the term in parentheses, which means minimize its negation
     #   this entire negated term is the cost (loss) to minimize
-    self._loss = -(Eq_log_pz - total_kl_qp + Eq_log_px - Eq_log_qz - scaled_disc_loss)
-    return self._loss
+    loss = -(Eq_log_pz - total_kl_qp + Eq_log_px - Eq_log_qz - scaled_disc_loss)
+    loss = tf.reduce_mean(loss, axis=0)
+    return loss
 
   @property
   def decoder(self):
     return self._decoder
 
   @property
-  def labels(self):
-    return self._labels
-
-  @property
   def hp(self):
     return self._hp
-
-  @property
-  def loss(self):
-    return self._loss
 
   @property
   def tau(self):
