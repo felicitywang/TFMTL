106e84d26b78aba7b7f963aae2a7a3e11ee2954c
felicitywang <cnfxwang@gmail.com>
2018-03-06 21:27:56 -0500

    sentiment_1/

---
+++ expts/sentiment_1/single.sh
@@ -0,0 +1,1 @@
+python ../scripts/discriminative_driver.py --datasets LMRD --dataset_paths data/tf/single/LMRD/min_50_max_-1/ --class_sizes 2 --vocab_path data/tf/single/LMRD/min_50_max_-1/vocab_size.txt --architectures_path architectures.json --model mult --alphas 1 --input_key tfidf --encoder_architecture no_op
