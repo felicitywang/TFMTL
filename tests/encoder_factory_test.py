3f7c5184c51ddc4836b9dc51154dcc2d60de1c2a
Seth Ebner <sebner1@jhu.edu>
2018-04-05 00:57:57 -0400

    WIP: Serial bi-RNN

--- tests/encoder_factory_test.py
+++ tests/encoder_factory_test.py
@@ -19,11 +19,11 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
-
 import tensorflow as tf
 
 from mtl.util.encoder_factory import build_encoders
 
+serial_lbirnn = False
 
 class EncoderTests(tf.test.TestCase):
   def test_template(self):
@@ -38,54 +38,99 @@ of tied/untied embedders and extractors are correct."""
     args = parser.parse_args()
 
     with self.test_session() as sess:
-      encoders = build_encoders(args)
+      if serial_lbirnn:
+        encoders = build_encoders(args)
+        inputs1 = tf.constant([[1,2,3],[4,5,6],[7,8,9]])
+        lengths1 = tf.constant([3,3,3])
 
-      inputs1 = tf.constant([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
-      lengths1 = tf.constant([3, 3, 3, 3])
+        inputs2 = tf.constant([[100,101,102,103],[104,105,106,107],[108,109,110,111]])
+        lengths2 = tf.constant([4,4,4])
 
-      k = dict()
+        #indices = tf.constant([1,2,0])
+        indices = None
+        inputs = [inputs1, inputs2]
+        lengths = [lengths1, lengths2]
+        output_SSTb = encoders['SSTb'](inputs=inputs, lengths=lengths, indices=indices)
+        output_LMRD = encoders['LMRD'](inputs=inputs, lengths=lengths, indices=indices)
 
-      ### COMMENT THESE LINES OUT IF NOT USING L-BIRNN EXTRACTOR
-      indices1 = tf.constant([1,1,2,0])
-      k = {'indices': indices1}
-      ###
+        all_variables = tf.global_variables()
+        trainable_variables = tf.trainable_variables()
 
-      output_SSTb_1 = encoders['SSTb'](inputs=inputs1, lengths=lengths1, **k)
-      output_LMRD_1 = encoders['LMRD'](inputs=inputs1, lengths=lengths1, **k)
+        init_ops = [tf.global_variables_initializer(),
+                    tf.local_variables_initializer()]
+        sess.run(init_ops)
 
-      inputs2 = tf.constant([[1, 1, 1], [2, 2, 0]])
-      lengths2 = tf.constant([3, 2])
-      output_SSTb_2 = encoders['SSTb'](inputs=inputs2, lengths=lengths2)
-      output_LMRD_2 = encoders['LMRD'](inputs=inputs2, lengths=lengths2)
+        all_var, train_var, s, l = sess.run([all_variables,
+                                          trainable_variables,
+                                             output_SSTb,
+                                             output_LMRD])
 
-      all_variables = tf.global_variables()
-      trainable_variables = tf.trainable_variables()
+        print('Encoders: {}'.format(encoders))
 
-      init_ops = [tf.global_variables_initializer(),
-                  tf.local_variables_initializer()]
-      sess.run(init_ops)
+        print('All variables created...')
+        for var in all_variables:
+          print(var)
 
-      all_var, train_var, s1, l1, s2, l2 = sess.run([all_variables,
-                                                     trainable_variables,
-                                                     output_SSTb_1,
-                                                     output_LMRD_1,
-                                                     output_SSTb_2,
-                                                     output_LMRD_2])
+        print('Trainable variables created...')
+        for var in trainable_variables:
+          print(var)
 
-      print('Encoders: {}'.format(encoders))
+        print(output_SSTb.eval())
 
-      print('All variables created...')
-      for var in all_variables:
-        print(var)
+        print('SSTb output size: {}'.format(output_SSTb.get_shape().as_list()))
+        print('LMRD output size: {}'.format(output_LMRD.get_shape().as_list()))        
 
-      print('Trainable variables created...')
-      for var in trainable_variables:
-        print(var)
 
-      print(output_SSTb_1.eval())
+        
+      else:
+        encoders = build_encoders(args)
 
-      print('SSTb_1 size: {}'.format(output_SSTb_1.get_shape().as_list()))
-      print('SSTb_2 size: {}'.format(output_SSTb_2.get_shape().as_list()))
+        inputs1 = tf.constant([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+        lengths1 = tf.constant([3, 3, 3, 3])
+
+        k = dict()
+
+        ### COMMENT THESE LINES OUT IF NOT USING L-BIRNN EXTRACTOR
+        indices1 = tf.constant([1,1,2,0])
+        k = {'indices': indices1}
+        ###
+
+        output_SSTb_1 = encoders['SSTb'](inputs=inputs1, lengths=lengths1, **k)
+        output_LMRD_1 = encoders['LMRD'](inputs=inputs1, lengths=lengths1, **k)
+
+        inputs2 = tf.constant([[1, 1, 1], [2, 2, 0]])
+        lengths2 = tf.constant([3, 2])
+        output_SSTb_2 = encoders['SSTb'](inputs=inputs2, lengths=lengths2)
+        output_LMRD_2 = encoders['LMRD'](inputs=inputs2, lengths=lengths2)
+
+        all_variables = tf.global_variables()
+        trainable_variables = tf.trainable_variables()
+
+        init_ops = [tf.global_variables_initializer(),
+                    tf.local_variables_initializer()]
+        sess.run(init_ops)
+
+        all_var, train_var, s1, l1, s2, l2 = sess.run([all_variables,
+                                                       trainable_variables,
+                                                       output_SSTb_1,
+                                                       output_LMRD_1,
+                                                       output_SSTb_2,
+                                                       output_LMRD_2])
+
+        print('Encoders: {}'.format(encoders))
+
+        print('All variables created...')
+        for var in all_variables:
+          print(var)
+
+        print('Trainable variables created...')
+        for var in trainable_variables:
+          print(var)
+
+        print(output_SSTb_1.eval())
+
+        print('SSTb_1 size: {}'.format(output_SSTb_1.get_shape().as_list()))
+        print('SSTb_2 size: {}'.format(output_SSTb_2.get_shape().as_list()))
 
 
 if __name__ == '__main__':
---
+++ tests/serial_lbirnn_test.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python
+# Copyright 2018 Johns Hopkins University. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+from mtl.extractors.lbirnn import serial_lbirnn
+
+class SerialLBiRNNTests(tf.test.TestCase):
+  def test_template(self):
+    with self.test_session() as sess:
+
+      i1 = tf.constant([[1,2,3],[4,5,6]], dtype=tf.float32)
+      i1 = tf.reshape(i1, [2,3,1])
+      l1 = tf.constant([3,3])
+      i2 = tf.constant([[7,8],[9,10]], dtype=tf.float32)
+      i2 = tf.reshape(i2, [2,2,1])
+      l2 = tf.constant([2,2])
+      i3 = tf.constant([[100],[101]], dtype=tf.float32)
+      i3 = tf.reshape(i3, [2,1,1])
+      l3 = tf.constant([1,1])
+      
+      inputs = [i1, i2, i3]
+      lengths = [l1, l2, l3]
+      indices = None
+      #indices = tf.constant([0,0])
+      num_layers = 2
+      #num_layers = 1
+      cell_type = tf.contrib.rnn.GRUCell
+      #cell_type = tf.contrib.rnn.BasicLSTMCell
+      cell_size = 32
+      initial_state_fwd = None
+      initial_state_bwd = None
+      outputs = serial_lbirnn(inputs,
+                              lengths,
+                              indices,
+                              num_layers,
+                              cell_type,
+                              cell_size,
+                              initial_state_fwd,
+                              initial_state_bwd)
+
+      all_variables = tf.global_variables()
+      trainable_variables = tf.trainable_variables()
+
+      init_ops = [tf.global_variables_initializer(),
+                  tf.local_variables_initializer()]
+      sess.run(init_ops)
+
+      all_var, train_var, outputs_ = sess.run([all_variables,
+                                               trainable_variables,
+                                               outputs])
+
+      print('All variables created...')
+      for var in all_variables:
+        print(var)
+
+      print('Trainable variables created...')
+      for var in trainable_variables:
+        print(var)
+
+      print(outputs_)
+      print(outputs_.shape)
+
+
+if __name__ == '__main__':
+  tf.test.main()
