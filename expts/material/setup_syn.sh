d845adb856759fe17ee5fc8d05a9d209b79b70e0
felicitywang <cnfxwang@gmail.com>
2018-10-09 19:33:06 -0400

    update syn data

--- expts/material/setup_syn.sh
+++ expts/material/setup_syn.sh
@@ -14,14 +14,14 @@
 # =============================================================================
 
 # synthetic data
-mkdir -p data/json/GOV_syn_p100r1000/
-mkdir -p data/json/LIF_syn_p100r1000/
-mkdir -p data/json/HEA_syn_p100r1000/
-mkdir -p data/json/BUS_syn_p100r1000/
-mkdir -p data/json/BUS_syn_p100r1000/
-mkdir -p data/json/LAW_syn_p100r1000/
-mkdir -p data/json/MIL_syn_p100r1000/
-mkdir -p data/json/SPO_syn_p100r1000/
+# mkdir -p data/json/GOV_syn_p100r1000/
+# mkdir -p data/json/LIF_syn_p100r1000/
+# mkdir -p data/json/HEA_syn_p100r1000/
+# mkdir -p data/json/BUS_syn_p100r1000/
+# mkdir -p data/json/BUS_syn_p100r1000/
+# mkdir -p data/json/LAW_syn_p100r1000/
+# mkdir -p data/json/MIL_syn_p100r1000/
+# mkdir -p data/json/SPO_syn_p100r1000/
 
 mkdir -p data/json/GOV_syn_p1000r1000/
 mkdir -p data/json/LIF_syn_p1000r1000/
@@ -32,33 +32,33 @@ mkdir -p data/json/LAW_syn_p1000r1000/
 mkdir -p data/json/MIL_syn_p1000r1000/
 mkdir -p data/json/SPO_syn_p1000r1000/
 
-mkdir -p data/json/GOV_syn_p11000r11000/
-mkdir -p data/json/LIF_syn_p11000r11000/
-mkdir -p data/json/HEA_syn_p11000r11000/
-mkdir -p data/json/BUS_syn_p11000r11000/
-mkdir -p data/json/BUS_syn_p11000r11000/
-mkdir -p data/json/LAW_syn_p11000r11000/
-mkdir -p data/json/MIL_syn_p11000r11000/
-mkdir -p data/json/SPO_syn_p11000r11000/
+# mkdir -p data/json/GOV_syn_p11000r11000/
+# mkdir -p data/json/LIF_syn_p11000r11000/
+# mkdir -p data/json/HEA_syn_p11000r11000/
+# mkdir -p data/json/BUS_syn_p11000r11000/
+# mkdir -p data/json/BUS_syn_p11000r11000/
+# mkdir -p data/json/LAW_syn_p11000r11000/
+# mkdir -p data/json/MIL_syn_p11000r11000/
+# mkdir -p data/json/SPO_syn_p11000r11000/
 
 
 
 
 # copy synthetic data
-cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/GOV/data.json.gz data/json/GOV_syn_p1000r1000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/LIF/data.json.gz data/json/LIF_syn_p1000r1000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/BUS/data.json.gz data/json/BUS_syn_p1000r1000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/LAW/data.json.gz data/json/LAW_syn_p1000r1000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/HEA/data.json.gz data/json/HEA_syn_p1000r1000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/MIL/data.json.gz data/json/MIL_syn_p1000r1000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/SPO/data.json.gz data/json/SPO_syn_p1000r1000/data.json.gz
+cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/clean/GOV/data.json.gz data/json/GOV_syn_p1000r1000/data.json.gz
+cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/clean/LIF/data.json.gz data/json/LIF_syn_p1000r1000/data.json.gz
+cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/clean/BUS/data.json.gz data/json/BUS_syn_p1000r1000/data.json.gz
+cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/clean/LAW/data.json.gz data/json/LAW_syn_p1000r1000/data.json.gz
+cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/clean/HEA/data.json.gz data/json/HEA_syn_p1000r1000/data.json.gz
+cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/clean/MIL/data.json.gz data/json/MIL_syn_p1000r1000/data.json.gz
+cp /export/a05/mahsay/domain/data/json/syn/p1000r1000/clean/SPO/data.json.gz data/json/SPO_syn_p1000r1000/data.json.gz
 
 
-cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/GOV/data.json.gz data/json/GOV_syn_p11000r11000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/LIF/data.json.gz data/json/LIF_syn_p11000r11000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/BUS/data.json.gz data/json/BUS_syn_p11000r11000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/LAW/data.json.gz data/json/LAW_syn_p11000r11000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/HEA/data.json.gz data/json/HEA_syn_p11000r11000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/MIL/data.json.gz data/json/MIL_syn_p11000r11000/data.json.gz
-cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/SPO/data.json.gz data/json/SPO_syn_p11000r11000/data.json.gz
+# cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/GOV/data.json.gz data/json/GOV_syn_p11000r11000/data.json.gz
+# cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/LIF/data.json.gz data/json/LIF_syn_p11000r11000/data.json.gz
+# cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/BUS/data.json.gz data/json/BUS_syn_p11000r11000/data.json.gz
+# cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/LAW/data.json.gz data/json/LAW_syn_p11000r11000/data.json.gz
+# cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/HEA/data.json.gz data/json/HEA_syn_p11000r11000/data.json.gz
+# cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/MIL/data.json.gz data/json/MIL_syn_p11000r11000/data.json.gz
+# cp /export/a05/mahsay/domain/data/json/syn/p11000r11000/SPO/data.json.gz data/json/SPO_syn_p11000r11000/data.json.gz
 
