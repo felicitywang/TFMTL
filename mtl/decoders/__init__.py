4bf4a0ee2188deb8fa6fc70464bd47fc915091a4
felicitywang <cnfxwang@gmail.com>
2019-04-16 17:27:10 -0400

    rm decoders

--- mtl/decoders/__init__.py
+++
@@ -1,6 +0,0 @@
-from mtl.decoders.cnn import cnn
-from mtl.decoders.cnn import shallow_cnn
-from mtl.decoders.dilated_conv import resnet
-from mtl.decoders.generic_decode import decode
-from mtl.decoders.rnn import rnn
-from mtl.decoders.unigram import unigram
